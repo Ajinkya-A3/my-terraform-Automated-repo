11b6d09b4de6d4415059588a6a997f3c8b0ba285
Ajinkya-A3 <ajinkyaa3xd@gmail.com>
2025-02-11 13:57:51 +0530

    format

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ resource "github_repository" "repo" {
 resource "null_resource" "git_push" {
   depends_on = [github_repository.repo]
 
-    provisioner "local-exec" {
-     command = "echo Initializing git repository... && git init && echo # ${var.repository_name} > README.md && git add . && git commit -m \"Initial-commit\" && git remote add origin ${github_repository.repo.git_clone_url} && git branch -M main && git push -u origin main"
+  provisioner "local-exec" {
+    command = "echo Initializing git repository... && git init && echo # ${var.repository_name} > README.md && git add . && git commit -m \"Initial-commit\" && git remote add origin ${github_repository.repo.git_clone_url} && git branch -M main && git push -u origin main"
 
 
 
     # Define the working directory for the command execution
-    working_dir = "${path.module}"
+    working_dir = path.module
 
     # Set up interpreter (use bash for Linux/macOS, PowerShell for Windows)
     interpreter = ["cmd", "/C"]
